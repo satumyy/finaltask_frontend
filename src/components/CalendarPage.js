9da8143a79bc1ffed85d5e854d360c18e0e86575
Satu Haapakoski <haapakoski.satu@gmail.com>
2021-05-20 19:02:31 +0300

    asiakasvarit

--- src/components/CalendarPage.js
+++ src/components/CalendarPage.js
@@ -17,8 +17,7 @@ function CalendarPage(){
       .then(data => setTrainings(data))
       .catch(err => console.err(err))
     }
-  
-    
+
     const fetchCustomers = () => {
       fetch('https://customerrest.herokuapp.com/api/customers')
       .then(response => response.json())
@@ -27,47 +26,20 @@ function CalendarPage(){
     }
 
 
-    console.log(JSON.stringify(trainings));
-/*
-    {"id":250,
-    "date":"2021-05-19T18:28:48.140+00:00",
-    "duration":60,"activity":"Fitness",
-    "customer":{"id":233,"firstname":"Mary","lastname":"Philips","streetaddress":"Hill Street 3","postcode":"23322","city":"Flintsone","email":"m.philips@mail.com","phone":"232-310123"}}
-    */
     let events = [];
     for(const training of trainings) {
         const endTime = new Date(Date.parse(training.date)+1000*60*training.duration).toISOString().substr(0,19)+"+00:00";
+        const customerColor = Math.floor(training.customer.id/400*16777215).toString(16);
         events.push({
             id: training.id,
-            color: '#fd3153',
+            color: '#' + customerColor.substring(1,5),
             from: training.date,
             to: endTime,
             title: training.activity + ' with ' + training.customer.firstname + ' ' + training.customer.lastname
         });
     }
 
-
-/*
-    const events = [{
-        id: 1,
-        color: '#fd3153',
-        from: '2021-05-02T18:00:00+00:00',
-        to: '2021-05-05T19:00:00+00:00',
-        title: 'This is an event'
-    }, {
-        id: 2,
-        color: '#1ccb9e',
-        from: '2021-05-01T13:00:00+00:00',
-        to: '2021-05-05T14:00:00+00:00',
-        title: 'This is another event'
-    }, {
-        id: 3,
-        color: '#3694DF',
-        from: '2019-05-05T13:00:00+00:00',
-        to: '2019-05-05T20:00:00+00:00',
-        title: 'This is also another event'
-    }];
-    */
+    console.log(events);
     
         
     return (
@@ -84,4 +56,5 @@ function CalendarPage(){
 }
 
 export default CalendarPage;
+
 
